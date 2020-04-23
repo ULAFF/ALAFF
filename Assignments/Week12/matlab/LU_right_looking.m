e5ca5e1ec886220abbf2cd92db149b24770537bd
Robert van de Geijn 2 <rvdg2@Maggies-MacBook-Air-3.local>
2020-04-23 13:49:07 -0500

    adding missing directory for Week12

---
+++ Assignments/Week12/matlab/LU_right_looking.m
@@ -0,0 +1,33 @@
+function [ A_out ] = LU_right_looking( A )
+
+  [ ATL, ATR, ...
+    ABL, ABR ] = FLA_Part_2x2( A, ...
+                               0, 0, 'FLA_TL' );
+
+  while ( size( ATL, 2 ) < size( A, 2 ) )
+
+    [ A00,  a01,     A02,  ...
+      a10t, alpha11, a12t, ...
+      A20,  a21,     A22 ] = FLA_Repart_2x2_to_3x3( ATL, ATR, ...
+                                                    ABL, ABR, ...
+                                                    1, 1, 'FLA_BR' );
+
+    %------------------------------------------------------------%
+
+    a21 = a21 / alpha11;
+    A22 = A22 - a21 * a12t;
+
+    %------------------------------------------------------------%
+
+    [ ATL, ATR, ...
+      ABL, ABR ] = FLA_Cont_with_3x3_to_2x2( A00,  a01,     A02,  ...
+                                             a10t, alpha11, a12t, ...
+                                             A20,  a21,     A22, ...
+                                             'FLA_TL' );
+
+  end
+
+  A_out = [ ATL, ATR
+            ABL, ABR ];
+
+end
